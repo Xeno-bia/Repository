57e170288ad9a9de8c559b9446b70e3a7d206315
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-22 00:59:50 +0900

    Add files via upload

--- Python/Calendar.py
+++ Python/Calendar.py
@@ -18,11 +18,13 @@ print(calendar.y)
 calendar.this_month()
 
 '''
-from X import *            # ライブラリ(拡張)
-class X:                   # クラス(設計図)
-    def __init__(self, X): #   変数(情報)
-    def X(self, X):        #   関数(機能)
-X = X(X)                   # オブジェクト(物)
-X.X                        #   変数(情報)
-X.X(X)                     #   関数(機能)
+from lib import *            # ライブラリ(拡張)
+
+class Cls():                 # クラス(設計図)
+    def __init__(self, arg): #   変数(情報)
+    def func(self, arg):     #   関数(機能)
+
+obj = Cls(arg)               # オブジェクト(物)
+obj.var                      #   変数(情報)
+obj.func(arg)                #   関数(機能)
 '''
