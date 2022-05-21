915ec049cd92fd03e672c0239ce954605ee86a88
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-21 19:35:50 +0900

    Add files via upload

--- Python/Calendar.py
+++ Python/Calendar.py
@@ -1,18 +1,15 @@
 print('''
-from calendar import *                        # 拡張機能calendarを使用
-from datetime import *                        # 拡張機能datetimeを使用
+from XXXX import *                            # 拡張機能XXXXを使用
 
-class Calendar:                               # カレンダーオブジェクトを作成
-    def this_month(self):                     # → 今月のカレンダー表示機能を作成
-        setfirstweekday(6)                    # → → 今月の開始曜日を設定
-        self.today = date.today()             # → → 今日の日付を取得
-        self.year = self.today.year           # → → 今日の年を取得
-        self.month = self.today.month         # → → 今日の月を取得
-        self.x = month(self.year, self.month) # → → カレンダーを作成
-        print(self.x)                         # → → カレンダーを表示
+class XXXX:                                   # XXXXオブジェクトを作成
+    def XXXX(self):                           # → XXXX機能を作成
+        XXXX                                  # → → XXXXを設定
+        XXXX                                  # → → XXXXを作成
+        XXXX                                  # → → XXXXを取得
+        print(XXXX)                           # → → XXXXを表示
 
-calendar = Calendar()                         # カレンダーオブジェクトを取得
-calendar.this_month()                         # 今月のカレンダー表示機能を使用
+XXXX = XXXX()                                 # XXXXオブジェクトを取得
+XXXX.XXXX()                                   # XXXX機能を使用
 ''')
 
 from calendar import *                        # 拡張機能calendarを使用
