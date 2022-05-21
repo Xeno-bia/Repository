9c12b90c1d2fc4e8bd6ec08ba93e78e2f813712e
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-22 04:26:45 +0900

    Add files via upload

--- Python/obj.py
+++ Python/obj.py
@@ -1,79 +1,48 @@
-py       #インタプリタ起動
-CTRL + C #インタプリタ終了
-py x.py  #ファイル実行
+'''
+> py program.py              # execution
 
-#x #コメント
+#comment                     # comment
 
-True, False #真偽値
-x           #整数
-x.x         #小数
-f'x'        #文字列
-[x]         #リスト
-{x: x}      #辞書
-x[x: x: x]  #抽出
+True, False                  # boolean
+0                            # integer
+0.0                          # floating point number
+range(start, stop, step)     # range
+f'string'                    # string
+[value]                      # list
+{key: value}                 # dictionary
+container[start: stop: step] # slice
 
-bool(x)        #真偽値
-int(x)         #整数
-float(x)       #小数
-str(x)         #文字列
-list(x)        #リスト
-dict(x)        #辞書
-range(x, x, x) #連番
+bool(object)                 # bool
+int(object)                  # int
+float(object)                # float
+str(object)                  # str
+list(object)                 # list
 
-if x: #分岐
-    x
-elif x:
-    x
-else:
-    x
+if condition:                # branch
+elif condition:              # branch
+else:                        # branch
 
-x + x   #加算・連結
-x - x   #減算
-x * x   #乗算・繰り返し
-x ** x  #冪乗
-x / x   #除算
-x // x  #整数除算
-x % x   #剰余算
-x == x  #等しい
-x < x   #小なり
-x <= x  #小なりイコール
-x > x   #大なり
-x >= x  #大なりイコール
-x in x  #含む
-x and x #かつ
-x or x  #または
-not x   #ではない
+x + x                        # addition
+x - x                        # subtraction
+x * x                        # multiplication
+x ** x                       # power
+x / x                        # division
+x % x                        # modulo
+x == x                       # equal
+x < x                        # less than
+x <= x                       # less than or equal
+x > x                        # greater than
+x >= x                       # greater than or equal
+x in x                       # in
+x and x                      # and
+x or x                       # or
+not x                        # not
 
-for i in x:  #コンテナループ
-    x
-    continue
-    break
-else:
-    x
-while x:     #条件ループ
-    x
-    continue
-    break
-else:
-    x
+for variable in container:   # loop
+    continue                 # continue
+    break                    # break
 
-def x(*x): #関数
-    x
-    return x
-
-x(x)
-
-class x(x): #クラス
-    def __init__(self, *x):
-        self.x = x
-
-    def x(self, *x):
-        x
-        super().x(x)
-        return x
-
-x(x)
-x.x
-x.x(x)
-
-from x import * #ライブラリ
+while condition:             # loop
+    continue                 # continue
+    break                    # break
+'''
---
+++ Python/template.py
@@ -0,0 +1,11 @@
+'''
+from library import *             # library
+
+class Class(super):               # class
+    def __init__(self, argument): #   variable
+    def method(self, argument):   #   method
+
+object = Class(argument)          # object
+object.variable                   #   variable
+object.method(argument)           #   method
+'''
