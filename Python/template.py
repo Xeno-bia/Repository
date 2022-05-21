37f42d933c9e11506bb00684d07dea6d8ddefe05
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-22 04:57:57 +0900

    Add files via upload

--- Python/template.py
+++ Python/template.py
@@ -9,3 +9,46 @@ object = Class(argument)          # object
 object.variable                   #   variable
 object.method(argument)           #   method
 '''
+
+'''
+#comment                     # comment
+
+True, False                  # boolean
+0                            # integer
+0.0                          # floating point number
+range(start, stop, step)     # range
+f'string'                    # string
+[value]                      # list
+{key: value}                 # dictionary
+container[start: stop: step] # slice
+
+x + x                        # addition
+x - x                        # subtraction
+x * x                        # multiplication
+x ** x                       # power
+x / x                        # division
+x % x                        # modulo
+x == x                       # equal
+x < x                        # less than
+x <= x                       # less than or equal
+x > x                        # greater than
+x >= x                       # greater than or equal
+x in x                       # in
+x and x                      # and
+x or x                       # or
+not x                        # not
+
+if condition:                # branch
+elif condition:              # branch
+else:                        # branch
+
+for variable in container:   # loop
+    continue                 # continue
+    break                    # break
+
+while condition:             # loop
+    continue                 # continue
+    break                    # break
+
+> py program.py              # execution
+'''
