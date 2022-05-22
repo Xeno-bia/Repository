cc515f4e79a0c5e9722419feef9d0ce52bb619ab
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-22 19:41:37 +0900

    Add files via upload

---
+++ Python/obj.py
@@ -0,0 +1,59 @@
+'''
+import calendar
+import time
+import datetime
+import fractions
+import numpy
+import scipy
+import pandas
+import matplotlib
+import os
+import sys
+import random
+import re
+import sklearn
+import torch
+import PIL
+import openpyxl
+import cv2
+import PyInstaller
+import pip
+import pip_review
+import PySimpleGUI
+import janome
+import bottle
+import flask
+import django
+import tensorflow
+import pygame
+import seaborn
+import bs4
+import chainer
+import email
+import googletrans
+import itertools
+import keras
+import logging
+import MeCab
+import playsound
+import statistics
+import sympy
+import urllib
+import webbrowser
+'''
+
+import math
+print(
+math.inf,          # 無限大
+math.pi,           # 円周率
+math.e,            # ネイピア数
+math.sin(5),       # サイン
+math.tan(5),       # コサイン
+math.cos(5),       # タンジェント
+math.radians(5),   #
+math.factorial(5), #
+math.gcd(5, 5),    #
+math.lcm(5, 5),    #
+math.log(5, 5),    #
+sep = '\n'
+)
