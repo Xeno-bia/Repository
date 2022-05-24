2fee9fde568a399cdcf8dd38714a9cf8ab26808f
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-25 04:43:06 +0900

    Add files via upload

--- Python/obj.py
+++ Python/obj.py
@@ -1,6 +1,5 @@
 '''
 import scipy
-import pandas
 import matplotlib
 import re
 import sklearn
@@ -20,11 +19,90 @@ import bs4
 import chainer
 import email
 import googletrans
-import itertools
 import keras
 import logging
 import MeCab
 import playsound
-import statistics
-import sympy
 '''
+inf             #無限大
+pi              #円周率
+e               #ネイピア数
+sin(x)       #サイン
+tan(x)       #コサイン
+cos(x)       #タンジェント
+factorial(x)                                                        #階乗
+gcd(x, x)                                                           #最大公約数
+lcm(x, x)                                                           #最小公倍数
+log(x, x)                                                           #対数
+mean(x)                                                             #平均
+median(x)                                                           #中央値
+mode(x)                                                             #最頻値
+pvariance(x)                                                        #分散
+pstdev(x)                                                           #標準偏差
+choice(x)                                                           #選択
+shuffle(x)                                                          #シャッフル
+randint(x, x)                                                       #乱数
+sleep(x)                                                            #待機
+calendar(x)                                                         #年間
+month(x, x)                                                         #月間
+now = now()                                                         #現在日時
+now.year                                                            #年
+now.month                                                           #月
+now.day                                                             #日
+now.hour                                                            #時
+now.minute                                                          #分
+now.second                                                          #秒
+datetime(x, x, x, x, x, x)                                          #日時
+date(x, x, x)                                                       #日付
+time(x, x, x)                                                       #時刻
+timedelta(weeks = x, days = x, hours = x, minutes = x, seconds = x) #時差
+Fraction(x)                                                      #分数
+platform                                                            #OS
+version                                                             #Pythonバージョン
+argv                                                                #ファイル・コマンドライン引数
+system(x)                                                           #コマンド
+x = get(x)                                                          #ブラウザ
+x.open()                                                           #開く
+x.open_new_tab()                                                   #新規タブで開く
+array(x)                                                            #numpy配列を作成
+x = DataFrame(x)                                             #データフレーム
+x[x]                                                             #行呼び出し
+x[x]                                                                #列呼び出し
+x.loc[x]                                                         #行列呼び出し
+x.query(x)                                                          #条件呼び出し
+x.sort_values(x)                                               #並べ替え
+symbols(x)                                                          #変数
+solve(x)                                                            #方程式
+expand(x)                                                           #展開
+factor(x)                                                           #因数分解
+diff(x)                                                             #微分
+integrate(x)                                                        #積分
+x.count(x)        #個数
+x.startswith(x)   #先頭判定
+x.endswith(x)     #末尾判定
+x.isalnum()      #英数字判定
+x.isdecimal()    #数字判定
+x.isalpha()      #英字判定
+x.isupper()      #大文字判定
+x.islower()      #小文字判定
+x.istitle()      #パスカル判定
+x.replace(x)      #置換
+x.split(x)        #分割
+x.upper()        #大文字化
+x.lower()        #小文字化
+x.title()        #パスカル化
+x.index()        #位置
+x.zfill()        #ゼロ埋め
+abs(x)            #絶対値
+all(x)       #論理積
+any(x)       #論理和
+len(x)       #長さ
+max(x)       #最大値
+min(x)       #最小値
+round(x)       #偶数丸め
+sorted(x)   #並べ替え
+sum(x)     #合計
+enumerate(x) #連番付
+combinations(x) #組み合わせ
+permutations(x)#順列
+#pyinstaller Pythonファイル --onefile --noconsole --icon = アイコン
