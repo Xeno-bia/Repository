02e062f41c1e314bdadd9a0abbde78d5569cbd00
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-07-16 18:44:25 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿https://visualstudio.microsoft.com/ja/downloads/
+csc *.cs
+.\*.exe
+class Program
+{
+    static void Main(string[] args)
+    {
+        //コメント
+        整数
+        小数
+        "文字列"
+        var 変数 = 値;
+        変数 = 値;
+        変数
+        var リスト = new List<型>();
+        リスト.Count
+        リスト.Add(値);
+        リスト.RemoveAt(インデックス);
+        リスト.Sort();
+        リスト.Reverse();
+        リスト.Exists(値);
+        リスト[インデックス]
+        + - * / %
+        == != < <= > >=
+        && || !
+        if (条件)
+        {
+            処理
+        }
+        else if (条件)
+        {
+            処理
+        }
+        else
+        {
+            処理
+        }
+        for (var 変数 = 開始; 変数 <= 終了; 変数++)
+        {
+            処理
+            continue;
+            break;
+        }
+        whlie (条件)
+        {
+            処理
+            continue;
+            break;
+        }
+        System.Console.ReadLine();
+        System.Console.WriteLine(値);
+        形態素解析
+        数学
+        スクレイピング
+        webアプリ
+        組み込み
+        画像
+        形態素解析
+        翻訳
+        分数
+        統計
+        機械学習
+        GUI
+        ゲーム
+        一括更新
+        Excel
+        GUI
+        正規表現
+        乱数
+        メール
+        ブラウザ
+    }
+}
