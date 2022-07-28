beafc811c5536c8f4c8efc80b86135bb19a38def
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-07-28 20:43:48 +0900

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,104 @@
+# コメント
+
+int
+float
+str
+
+var
+list
+dict
+
++
+-
+*
+**
+/
+//
+%
+
+==
+!=
+<
+<=
+>
+>=
+
+and
+or
+not
+
+if 条件
+    処理
+elif 条件
+    処理
+else
+    処理
+
+for 変数 in 配列
+    処理
+    continue
+    break
+
+while 条件
+    処理
+    continue
+    break
+
+> pip list
+> pip install ライブラリ
+> pip install -U ライブラリ
+from ライブラリ import *
+MeCab       # 自然言語処理
+janome      # 自然言語処理
+tokenize    # 形態素解析
+matplotlib  # グラフ
+math        # 計算
+numpy       # 計算
+requests    # Webスクレイピング
+bs4         # Webスクレイピング
+pytube      # YouTubeスクレイピング
+twint       # Twitterスクレイピング
+icrawler    # 画像スクレイピング
+bottle      # Webアプリ
+builtins    # 組み込み
+    input("プロンプト")
+    print()
+PIL         # 画像処理
+csv         # 画像処理
+keras       # ディープラーニング
+googletrans # 翻訳
+fractions   # 分数
+flask       # Webアプリ
+django      # Webアプリ
+datetime    # 日時
+dateutil    # 日時
+time        # 時間
+calendar    # カレンダー
+sys         # インタプリタ
+sympy
+subprocess  # コマンド
+statistics  # 統計
+PyCaret     # 機械学習
+sklearn     # 機械学習
+shutil
+seaborn
+scipy       # 科学技術計算
+PySimpleGUI # GUIアプリ
+tkinter     # GUIアプリ
+PyInstaller # exe化
+pygame      # ゲーム
+playsound
+pip_review  # ライブラリ一括更新
+pandas      # データ分析
+os          # フォルダ・ファイル
+openpyxl    # Excel
+itertools   # 組み合わせ
+importlib
+urllib
+tensorflow  # ディープラーニング
+re          # 正規表現
+random      # 乱数
+email       # メール
+webbrowser  # ブラウザ
+json        # json
+pickle      # オブジェクト保存
